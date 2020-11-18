bc7ef5b674c1ac6d2e60b2e5c2ec97c41424f415
xghell <605301193@qq.com>
2020-11-18 23:40:15 +0800

    添加列表（xg-list)组件

---
+++ xg-list/xg-list.vue
@@ -0,0 +1,30 @@
+<template>
+	<!-- #ifdef APP-NVUE -->
+	<list :show-scrollbar="showScrollbar">
+		<slot></slot>
+	</list>
+	<!-- #endif -->
+	
+	<!-- #ifndef APP-NVUE -->
+	<scroll-view :show-scrollbar="showScrollbar" scroll-y="true" >
+		<view>
+			<slot></slot>
+		</view>
+	</scroll-view>
+	<!-- #endif -->
+</template>
+
+<script>
+	export default {
+		props: {
+			showScrollbar: {
+				type: Boolean,
+				default: true
+			},
+		},
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
