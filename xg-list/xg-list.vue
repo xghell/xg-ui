fa5de882bf0ec43f1e5b45332e781b8da70900a9
xghell <605301193@qq.com>
2020-12-17 23:37:22 +0800

    优化列表组件xg-list（增加滚动事件scroll；增加scroll-top属性）

--- xg-list/xg-list.vue
+++ xg-list/xg-list.vue
@@ -1,12 +1,12 @@
 <template>
 	<!-- #ifdef APP-NVUE -->
-	<list :id="id" :show-scrollbar="showScrollbar">
+	<list :id="id" :show-scrollbar="showScrollbar" :scroll-top="scrollTop" @scroll="onScroll">
 		<slot></slot>
 	</list>
 	<!-- #endif -->
 	
 	<!-- #ifndef APP-NVUE -->
-	<scroll-view :id="id" class="list" :show-scrollbar="showScrollbar" scroll-y="true" >
+	<scroll-view :id="id" class="list" :show-scrollbar="showScrollbar" scroll-y="true" :scroll-top="scrollTop" @scroll="onScroll">
 		<view>
 			<slot></slot>
 		</view>
@@ -15,6 +15,9 @@
 </template>
 
 <script>
+	/**
+	 * @event {Function()} scroll 
+	 */
 	export default {
 		props: {
 			id: {
@@ -24,6 +27,36 @@
 				type: Boolean,
 				default: true
 			},
+			scrollTop: {
+				type: Number,
+				default: 0,
+			}
+		},
+		methods: {
+			onScroll(e) {
+				// #ifndef APP-NVUE
+				const payload = {
+					detail: {
+						scrollWidth: e.detail.scrollWidth,
+						scrollHeight: e.detail.scrollHeight,
+						scrollLeft: e.detail.scrollLeft,
+						scrollTop: e.detail.scrollTop
+					}
+				}
+				// #endif
+				// #ifdef APP-NVUE
+				const payload = {
+					detail: {
+						scrollWidth: e.contentSize.width,
+						scrollHeight: e.contentSize.height,
+						scrollLeft: -e.contentOffset.x,
+						scrollTop: -e.contentOffset.y,
+					}
+				}
+				// #endif
+				
+				this.$emit('scroll', payload);
+			}
 		},
 	}
 </script>
